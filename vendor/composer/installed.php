b122b2bfeea51d3c206f8666f62f9a2812456b41
Mohammad Al Saleh <mo.alsaleh95@gmail.com>
2022-11-24 15:00:52 +0300

    fixed padding

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'jackocnr/intl-tel-input',
+        'pretty_version' => '17.0.19',
+        'version' => '17.0.19.0',
+        'reference' => NULL,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'jackocnr/intl-tel-input' => array(
+            'pretty_version' => '17.0.19',
+            'version' => '17.0.19.0',
+            'reference' => NULL,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.6.5',
+            'version' => '6.6.5.0',
+            'reference' => '8b6386d7417526d1ea4da9edb70b8352f7543627',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
