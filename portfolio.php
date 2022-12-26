8297b3b75eb15decc31fc2b77d914da5f7e852fa
Enes Demir <enesdemir12@outlook.com.tr>
2022-12-26 11:03:35 +0300

    fixed bugs

--- portfolio.php
+++ portfolio.php
@@ -1,6 +1,9 @@
 <?php include('inc/config.php'); ?>
+
 <?php include('lang/tr.php'); ?>
-<?php include('components/layouts/header-portfolio.php');?>
+
+<?php include('components/layouts/header-portfolio.php'); ?>
 
 <?php include('components/portfolio.php'); ?>
+
 <?php include('components/layouts/footer.php'); ?>
