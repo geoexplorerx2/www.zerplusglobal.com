833092859cb22ad017f3d3d6f455c1f3a6b80ad8
Adnane SOUGMI <adnane.sougmi@gmail.com>
2023-07-26 15:45:15 +0300

    add GTM

--- components/layouts/header.php
+++ components/layouts/header.php
@@ -41,9 +41,19 @@
     <link rel="stylesheet" href="<?= $pageLink ?>/css/style.css"/>   
     <link rel="stylesheet" href="<?= $pageLink ?>/css/demo.css"/>
     <!-- <lik rel="preload" href="<?= $pageLink ?>/css/demo.css" type="text/css" as="style" onload="this.onload=null;this.rel='stylesheet';" /> -->
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WXVWP836');</script>
+<!-- End Google Tag Manager -->
 </head>
 <body>
-
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WXVWP836"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 <div id="preloader">
     <div class="loading-text"><?= $lang['loading']; ?></div>
 </div>
